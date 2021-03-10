7475bc53c90628a6a784f0a05ff8bf970b531a2e
Ahmed khaled <ak812554@gmail.com>
2021-03-10 14:29:19 +0200

    modifications are complete

--- webpack.config.js
+++ webpack.config.js
@@ -6,115 +6,96 @@ module.exports = {
     entry: './src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
-        publicPath: './',
+        publicPath: '/',
         filename: 'js/bundle.js'
     },
     module: {
         rules: [
-
-           {
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+           },
+            {
                 test: /\.(sass|css|scss)$/i,
                 use: [
-                    
-                // Creates `style` nodes from JS strings
-           {
-                loader:MiniCssExtractPlugin.loader, 
-                options: {
-                    publicPath: "../",
-                }
-            },
-                // Translates CSS into CommonJS
-                "css-loader",
-                // Compiles Sass to CSS
-                "sass-loader",
+                    MiniCssExtractPlugin.loader, 
+                    "css-loader",
+                    "sass-loader",
                 ],
-           },
-
-           {
+            },
+            {
                 test: /\.(png|jpe?g|gif)$/i,
                 use: [
                     {
-                    loader: 'file-loader',
+                        loader: 'file-loader',
                         options: {
                             name:'[name].[ext]',
                             outputPath: 'images',
-                          
-                        }
+                        },
                     },
                 ],
-          },
-
-          {
-            test: /\.(ttf|svg|eot|woff|woff2|)$/i,
-            use: [
-                {
-                loader: 'file-loader',
-                    options: {
-                        name:'[name].[ext]',
-                        outputPath: 'fonts',
-                      
-                    }
-                },
-            ],
-      },
-      
-          {
-                test: /\.html$/i,
-                loader: 'html-loader',
-          },
-          {
-            test: require.resolve("jquery"),
-            loader: "expose-loader",
-            options: {
-              exposes: ["$", "jQuery"],
             },
-          },
-    
+            {
+                test: /\.(ttf|svg|eot|woff|woff2|)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name:'[name].[ext]',
+                            outputPath: 'fonts', 
+                        },
+                    },
+                ],
+            },
+            {
+                test: require.resolve("jquery"),
+                loader: "expose-loader",
+                options: {
+                  exposes: ["$", "jQuery"],
+                },
+              },
         ],
-    },
+    },    
     plugins: [
         new HtmlWebpackPlugin({
-        template: "./src/index.html",
-        filename: "index.html",
+            template: "./src/index.html",
+            filename: "index.html",
         }),
         new HtmlWebpackPlugin({
             template: "./src/projects.html",
             filename: "projects.html",
-            }),
-            new HtmlWebpackPlugin({
-                template: "./src/project-details.html",
-                filename: "project-details.html",
-                }),
-                new HtmlWebpackPlugin({
-                    template: "./src/blog.html",
-                    filename: "blog.html",
-                    }),
-                    new HtmlWebpackPlugin({
-                        template: "./src/blog-details.html",
-                        filename: "blog-details.html",
-                        }),
-                        new HtmlWebpackPlugin({
-                            template: "./src/add-blog.html",
-                            filename: "add-blog.html",
-                            }),
-                            new HtmlWebpackPlugin({
-                                template: "./src/about.html",
-                                filename: "about.html",
-                                }),
-                                new HtmlWebpackPlugin({
-                                    template: "./src/contact.html",
-                                    filename: "contact.html",
-                                    }),
-                    
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/project-details.html",
+            filename: "project-details.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/blog.html",
+            filename: "blog.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/blog-details.html",
+            filename: "blog-details.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/add-blog.html",
+            filename: "add-blog.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/about.html",
+            filename: "about.html",
+        }),
+        new HtmlWebpackPlugin({
+            template: "./src/contact.html",
+            filename: "contact.html",
+        }),
         new MiniCssExtractPlugin({filename: "css/style.css"}),
     ],
     devServer: {
         contentBase: path.join(__dirname, 'build'),
-        // compress: true,
         port: 9000,
         open:true,
         writeToDisk: true,
         stats: 'errors-only',
-       
     }
 };
